e8a195b822ff906c6d4f7ae7c006044c09600dd4
Yuki Miyake <zawawabis@gmail.com>
2015-08-17 05:43:54 +0900

    静的ファイルをpublicフォルダに移動
    サーバーが参照しやすくなるため。

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 //サーバー
+
+var express = require("express");
+var app = express();
+
+var http = require("http").Server(app);
+
+app.use(express.static("public"));
+app.set("port", (process.env.PORT || 5000));
+
+
+
+app.get("/", function (req, res) {
+    res.sendFile("index.html");
+});
+
+
+
+http.listen(app.get("port"), function () {
+    console.log("port = %s", app.get("port"));
+});
