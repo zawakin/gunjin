44a929a9ca48a0918f1b3856c8da84e67dfc8695
Yuki Miyake <zawawabis@gmail.com>
2015-08-17 06:29:34 +0900

    初期ページで対戦を希望している人のリストをクライアント側に表示するための準備

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require("express");
 var app = express();
 
 var http = require("http").Server(app);
-
+var io = require("socket.io")(http);
 app.use(express.static("public"));
 app.set("port", (process.env.PORT || 5000));
 
@@ -14,6 +14,15 @@ app.get("/", function (req, res) {
     res.sendFile("index.html");
 });
 
+var clients = [];
+
+io.on("connection", function (socket) {
+
+    socket.on("disconnect", function () {
+        console.log("client disconnected");
+    });
+
+});
 
 
 http.listen(app.get("port"), function () {
