554762c823ee670268181ecd40c67da2484e81ec
miyake yuki <zawawabis@gmail.com>
2015-10-08 01:30:58 +0900

    部屋ページの作成とマッチングシステムの実装、先後決定、ejsでテンプレート
    ejsを用いてテンプレートで対戦待機から対戦決定の通信を可能にした。

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-//�T�[�o�[
+﻿//サーバー
 
 var express = require("express");
 var app = express();
+var ejs = require("ejs");
 
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 app.use(express.static("public"));
 app.set("port", (process.env.PORT || 5000));
 
-
+app.engine("ejs",ejs.renderFile);
 
 app.get("/", function (req, res) {
     res.sendFile("index.html");
 });
 
-app.get("/game",function(req,res){
-	res.sendFile("battle.html");
+app.get("/game", function (req, res) {
+    res.render(__dirname + "/public/taisen.ejs",
+        {title:"taitoru", roomNum:req.query.r});
+
 });
+
+
 
 var clients = [];
 var games = [];
+
+var rooms = [];
+for(var i=1;i<=100;i++){
+    rooms[i] = {waiting:false,num:0, clients:[]};
+}
 
 function GetClientList() {
     var list = [];
@@ -42,6 +52,32 @@ io.on("connection", function (socket) {
     		}
     	}
         console.log("client disconnected");
+    });
+
+    socket.on("taisenmachi", function (data) {
+        var room = rooms[data.roomNum];
+        console.log(data.roomNum + ":" + data.name + " joined.");
+        if (room.clients.length == 0) {
+            room.clients[0] = { id: socket.id, name: data.name };
+            socket.join("room" + data.roomNum);
+            console.log(data.roomNum + ":" + data.name + " is waiting...");
+            room.waiting = true;
+        } else {
+            if (room.clients.length == 1) {
+                room.clients[1] = { id: socket.id, name: data.name };
+                socket.join("room" + data.roomNum);
+
+                room.waiting = false;
+                console.log(data.roomNum + ": matching success");
+                var sente = Math.floor(Math.random() * 2);
+                room.sente = room.clients[sente];
+                room.gote = room.clients[1 - sente];
+                console.log(data.roomNum + ": 先手:" + room.sente.name + " vs 後手:" + room.gote.name);
+                io.to("room" + data.roomNum).emit("taisenKettei", room);
+            }
+        }
+
+        //console.log(data);
 
     });
 
